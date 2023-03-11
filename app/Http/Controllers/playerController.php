f3748fa2633145af3f7d7f7c0c4b65f9480d3776
AbdullahRahmoun <aboodyggkol@gmail.com>
2023-03-11 17:33:29 +0300

    edited the show method

--- app/Http/Controllers/playerController.php
+++ app/Http/Controllers/playerController.php
@@ -2,11 +2,27 @@
 namespace App\Http\Controllers;
 
 use App\Models\Player;
+use Illuminate\Http\Request;
+
+use function PHPUnit\Framework\isNull;
 
 class playerController extends Controller
 {
-    public function show(Player $id)
+    public function show($id,Request $request)
     {
-        return $id;
+        if($id<0){
+            $user=$request->user();
+            //must change to relation later
+            // return $user->userName;
+            $player=Player::where('name',$user->userName)->First();
+            //end
+            if($player==null){
+                return response([
+                    'message'=>'this account doesnt belong to a player'
+                ],400);
+            }
+            return $player;
+        }
+        
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -22,10 +22,11 @@ Route::post('login',[userController::class,'login']);
 Route::post('signup',[userController::class,'signup']);
 Route::post('matchMaking',PartOneAutoMatchMaking::class);
 Route::post('excelInput',HandleExcelInput::class);
-Route::get('player/{id}',[playerController::class,'show']);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::middleware('auth:sanctum')
+->get('player/{id}',[playerController::class,'show']);
 Route::get('lsjfsd',function (){
     
 });
